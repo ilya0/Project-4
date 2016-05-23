d59a71567d273256bc40fd5cdeb528f28fdd56e2
ilya osovets <iosovets@gmail.com>
2016-05-23 16:58:44 -0700

    working code

--- app_logic.js
+++ app_logic.js
@@ -19,7 +19,7 @@ $(".testbutton").click(function(event){
 
 var $inputboxval = "";
 var listvizArray =["listvizArray0","listvizArray1","listvizArray2"]; //data for the list visualization
-var cloudvizArray=["cloudvizArray0","cloudvizArray1","cloudvizArray2"]; // data for the text cloud vix
+var cloudvizArray=["cloudvizArray0 fine","cloudvizArray1 ok","cloudvizArray2 ok"]; // data for the text cloud vix
 var pievizArray=["pivevizArray"];   //data for the pie (thats not here yet)
 //var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 var alphabet = listvizArray; //making the alphabet array the input array
@@ -145,25 +145,27 @@ function wordCloud(selector) {
 }
 //****** original words
 //Some sample data - http://en.wikiquote.org/wiki/Opening_lines
-// var words = [
-// "You don't know about me without you have read a book called The Adventures of Tom Sawyer but that ain't no matter.",
-//     "The boy with fair hair lowered himself down the last few feet of rock and began to pick his way toward the lagoon.",
-//     "When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.",
-//     "It was inevitable: the scent of bitter almonds always reminded him of the fate of unrequited love."
-// ]
+var words = [
+  "You don't know about me without you have read a book called The Adventures of Tom Sawyer but that ain't no matter.",
+  "The boy with fair hair lowered himself down the last few feet of rock and began to pick his way toward the lagoon.",
+  "When Mr. Bilbo Baggins of Bag End announced that he would shortly be celebrating his eleventy-first birthday with a party of special magnificence, there was much talk and excitement in Hobbiton.",
+  "It was inevitable: the scent of bitter almonds always reminded him of the fate of unrequited love."
+]
 
 var words = cloudvizArray
-
 //Prepare one of the sample sentences by removing punctuation,
 // creating an array of words and computing a random size attribute.
-function getWords(i) {
+function getWords() {
     return words[i]
             .replace(/[!\.,:;\?]/g, '')
             .split(' ')
             .map(function(d) {
                 return {text: d, size: 10 + Math.random() * 60};
             })
+
 }
+
+
 
 //This method tells the word cloud to redraw with a new set of words.
 //In reality the new words would probably come from a server request,
@@ -172,7 +174,7 @@ function showNewWords(vis, i) {
     i = i || 0;
 
     vis.update(getWords(i ++ % words.length))
-    setTimeout(function() { showNewWords(vis, i + 1)}, 4000)  //this is the timer function to rerun the update
+     setTimeout(function() { showNewWords(vis, i + 1)}, 4000)  //this is the timer function to rerun the update
 }
 
 //Create a new instance of the word cloud visualisation.
@@ -180,4 +182,6 @@ var myWordCloud = wordCloud('body');
 
 //Start cycling through the demo data
 showNewWords(myWordCloud); // this runs the whole update
+
+
 
