0ab17dcf872bcb5fa507946f32bc6fbc873fcd02
ilya osovets <iosovets@gmail.com>
2016-05-24 20:16:45 -0700

    working push to front end, still needs to refine the data to artist names only

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,16 @@ var app = express();
 var port = process.env.PORT || 3000;
 var twitterRoutes = require('./routes/twitter_routes');
 var Twit = require('twit');
+var path = require('path');
 
+// all requests to static files, app looks here
+app.use(express.static(path.join(__dirname, 'Frontend')))
 
+app.get('/',function(req,res){
+  // combine the path from the root of the server all the into the project directory
+  // append 'Frontend/index_init_1.html' to it
+  res.sendFile(path.join(__dirname, 'Frontend/index_init_1.html'));
+});
 app.use('/api', twitterRoutes);
 
 
